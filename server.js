8a1ed076fee87a8e4f9b0c55fa8b3058894f098c
GuiBret <guillaume.bretzner@gmail.com>
2019-12-11 01:10:17 +0100

    ajout event + fix cors

--- server.js
+++ server.js
@@ -1,1 +1,36 @@
- 
+const express = require('express'),
+      app = express(),
+      http = require('http').Server(app),
+      io = require('socket.io')(http);
+app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+    
+        // authorized headers for preflight requests
+        // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        next();
+    
+        app.options('*', (req, res) => {
+            // allowed XHR methods  
+            res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+            res.send();
+        });
+    });
+
+
+io.on('connection', socket => {
+    let previousID;
+
+    // const joinFct = currentId => {
+    //     socket.leave(previousID);
+    //     socket.join(currentId);
+
+    //     previousId = currentId;
+    // }
+
+    socket.on('checkboxSelected', checkbox_id => {
+        socket.emit("checkboxModified", checkbox_id);
+    });
+});
+
+io.listen(3001);
